f34d7c7f6ca95549d74dc8de55cc3c4d47ecbd86
tyres9 <tyres9@yahoo.com>
2025-03-29 21:06:03 +0900

    Adding Forms and Templates

--- website/views.py
+++ website/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html',{})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        #authenticate
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, "You have been logged in.")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an Error logging in.")
+            return redirect('home')
+    else:        
+        return render(request, 'home.html',{})
+
+def login_user(request):
+    pass
+    
+ 
+ 
+def logout_user(request):
+    logout(request)
+    messages.success(request,"You have been logged out")
+    return redirect('home')        
+
+def register_user(request):
+    return render(request, 'register.html',{})
+    
