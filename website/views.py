31d0612e425154981084bf58b2adca7f2f1b5290
tyres9 <tyres9@yahoo.com>
2025-04-06 21:51:23 +0900

    Adding a view record page

--- website/views.py
+++ website/views.py
@@ -51,3 +51,12 @@ def register_user(request):
         return render(request, 'register.html',{'form':form})
     
     return render(request, 'register.html',{'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #look for records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html',{'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view the page....")
+        return redirect('home')
