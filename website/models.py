c524f6777b2b98f1fb0f70c2ad219219eceb35fc
tyres9 <tyres9@yahoo.com>
2025-04-06 14:22:40 +0900

    edit home.html

--- website/models.py
+++ website/models.py
@@ -9,7 +9,6 @@ class Record(models.Model):
     address = models.CharField(max_length=100)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=20)
     
     def __str__(self):
--- website/views.py
+++ website/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+    
+    
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +22,7 @@ def home(request):
             messages.success(request, "There was an Error logging in.")
             return redirect('home')
     else:        
-        return render(request, 'home.html',{})
+        return render(request, 'home.html',{'records' : records})
 
 def login_user(request):
     pass
